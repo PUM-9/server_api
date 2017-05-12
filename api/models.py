288c79b20008ed93f8244d5f6ad48c9447b5b7b3
Hampus Dunström <hampusdunstrom@gmail.com>
2017-05-12 11:40:06 +0200

    started on the database models and a create register job function

--- api/models.py
+++ api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from enum import Enum
+from django.utils import timezone
 
-# Create your models here.
+
+class LogLevel(Enum):
+    TRACE = 0
+    DEBUG = 1
+    INFO = 2
+    WARNING = 3
+    ERROR = 4
+
+
+class Job(models):
+    created = models.DateTimeField('created')
+    started = models.DateTimeField('started', null=True)
+    finished = models.DateTimeField('finished', null=True)
+    job_type = models.CharField('job_type', max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    log_level = models.SmallIntegerField('log_level', default=LogLevel.WARNING)
+
+
+class Registration(Job):
+    max_correspondence = models.FloatField('max_correspondence')
+    max_iterations = models.IntegerField('max_iterations')
+    transformation_epsilon = models.FloatField('transformation_epsilon')
+    leaf_size = models.FloatField('leaf_size')
+
+    @classmethod
+    def create_from_json(cls, arguments):
+        print(arguments)
+
+
+class Mesh(Job):
+    pass
+
+
+class File(models):
+    created = models.DateTimeField('created')
+    path = models.CharField('path', max_length=300)
+    file_type = models.CharField('file_type', max_length=100)
+    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+
+class User(models):
+    username = models.CharField('username')
+
--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from api.models import Registration
+import json
 
-# Create your views here.
+type_json = 'application/json'
+
+
+def create_register_job(request):
+
+    if request.method == 'GET':
+        context = json.dumps({'status': 400, 'reason': 'Bad request'})
+        response = HttpResponse(context, content_type=type_json)
+        response.status_code = 400
+        return response
+
+    arguments = json.dumps(request.body.decode())
+    job_created = Registration.create(arguments)
+    if job_created:
+        return HttpResponse('')
+    else:
+        context = json.dumps({'status': 406, 'reason': 'Job could not be created'})
+        response = HttpResponse(context, content_type=type_json)
+        response.status_code = 406
+        return response
