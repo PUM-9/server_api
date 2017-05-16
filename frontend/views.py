a619c27156e8049f0359e3705768462554ba6fc2
Hampus Dunström <hampusdunstrom@gmail.com>
2017-05-16 12:53:56 +0200

    registration form working.

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.urls import reverse
-from django.views.generic.edit import FormView
 from jobs.forms import RegistrationJobForm
 from jobs.models import File, Registration
 from django.views.decorators.csrf import csrf_protect
@@ -14,24 +12,25 @@ def index(request):
 
 @csrf_protect
 def registration_job_form(request):
-    form = RegistrationJobForm()
     if request.method == 'POST':
+        form = RegistrationJobForm(request.POST)
+        form.fields['files'].required = False
         files = request.FILES.getlist('files')
-        if form.is_valid():
+        if form.is_valid() and files:
             name = form.cleaned_data['name']
             log_level = form.cleaned_data['log_level']
             max_correspondence = form.cleaned_data['max_correspondence']
             max_iterations = form.cleaned_data['max_iterations']
             transformation_epsilon = form.cleaned_data['transformation_epsilon']
             leaf_size = form.cleaned_data['leaf_size']
-            job_id = Registration.create(name, log_level, max_correspondence, max_iterations, transformation_epsilon,
-                                         leaf_size)
+            job = Registration.create(name, log_level, max_correspondence, max_iterations, transformation_epsilon,
+                                      leaf_size)
             for f in files:
-                File.create(f, job_id)
+                File.create(f, job)
             return render(request, 'frontend/index.html')
         else:
-            print(form.errors)
             print("form invalid")
             return render(request, 'frontend/index.html')
     else:
+        form = RegistrationJobForm()
         return render(request, 'frontend/registration_form.html', {'form': form})
--- jobs/forms.py
+++ jobs/forms.py
@@ -2,14 +2,15 @@ from django import forms
 
 
 class RegistrationJobForm(forms.Form):
-    name = forms.CharField(max_length=200)
+    name = forms.CharField(max_length=200, label="Name")
     log_level = forms.ChoiceField(choices=(("0", "Trace"),
                                            ("1", "Debug"),
                                            ("2", "Info"),
                                            ("3", "Warning"),
-                                           ("4", "Error")), initial='2')
-    max_correspondence = forms.DecimalField(min_value=0, initial=10)
-    max_iterations = forms.IntegerField(min_value=1, max_value=200, initial=100)
-    transformation_epsilon = forms.DecimalField(min_value=0, max_value=10, initial=0.00000001)
-    leaf_size = forms.DecimalField(min_value=0, max_value=100, initial=0.43)
-    files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+                                           ("4", "Error")), initial='2', label="Log level")
+    max_correspondence = forms.DecimalField(min_value=0, initial=10, label="Max Correspondence")
+    max_iterations = forms.IntegerField(min_value=1, max_value=200, initial=100, label="Max iterations")
+    transformation_epsilon = forms.DecimalField(min_value=0, max_value=10, initial=0.00000001,
+                                                label="Transformation Epsilon")
+    leaf_size = forms.DecimalField(min_value=0, max_value=100, initial=0.43, label="Leaf Size")
+    files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), label="Point Clouds")
